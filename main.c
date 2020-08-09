6c086f8dc927e20254b3541c7912220aaf9fe56f
Swapnil <59367604+desaiswapnil92@users.noreply.github.com>
2020-08-09 14:07:16 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int BinaryConversion (int User_Input)
+{
+    int Original = User_Input, Remainder, arr[10], count =0, i;
+    while(User_Input>0){
+        Remainder = User_Input%2;
+        User_Input = User_Input/2;
+        arr[count++] = Remainder;
+    }
+    printf("\n Binary of number %d is : ",Original);
+    for (i=count-1; i>=0; i--)
+    {
+        printf("%d",arr[i]);
+    }
+    return 0;
+}
+
+int main()
+{
+    int User_Input=0 ;
+    printf("Please enter the number to convert into Binary  : ");
+    scanf("%d",&User_Input);
+    BinaryConversion(User_Input);
+}
